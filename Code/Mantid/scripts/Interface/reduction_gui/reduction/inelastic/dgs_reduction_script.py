d738c1a302bf42aeb3c5c0cc04fe8c9a419392ae
Russell Taylor <taylorrj@ornl.gov>
2012-10-02 15:51:19 -0400

    Re #5896. Add hardcoded HYSPEC example.

--- Code/Mantid/scripts/Interface/reduction_gui/reduction/inelastic/dgs_reduction_script.py
+++ Code/Mantid/scripts/Interface/reduction_gui/reduction/inelastic/dgs_reduction_script.py
@@ -44,4 +44,25 @@ class DgsReductionScripter(BaseReductionScripter):
         return script
     
     def to_live_script(self):
-        raise RuntimeError, "Not done yet!"
+        script = "from mantidsimple import *\n"
+        script += """StartLiveData(UpdateEvery='5',Instrument='FileEventDataListener',StartTime='2012-09-04T15:58:03',ProcessingScript="Rebin(InputWorkspace=input,OutputWorkspace='reb',Params=(5000,500,35000))\\nDgsReduction(SampleInputWorkspace=input,OutputWorkspace=output,IncidentEnergyGuess=25,UseIncidentEnergyGuess=1,EnergyTransferRange=(-20,0.5,20),SofPhiEIsDistribution=0,RejectZeroBackground=0)",PreserveEvents='1',PostProcessingScript="GroupDetectors(InputWorkspace=input,OutputWorkspace=input,MapFile=r'/home/tr9/HYS/HYS_Grouping_2x.xml',Behaviour='Average')\\nTranspose(InputWorkspace=input,OutputWorkspace='Ephi')\\nSofQW3(InputWorkspace=input,OutputWorkspace=input,QAxisBinning='2,0.1,7',EMode='Direct',EFixed='25')\\nTranspose(InputWorkspace=input,OutputWorkspace=output)\\nPlus(LHSWorkspace='reb2',RHSWorkspace='reb',OutputWorkspace='reb2') if mtd.workspaceExists('reb2') else CloneWorkspace(InputWorkspace='reb',OutputWorkspace='reb2')\\nSumSpectra(InputWorkspace='reb2',OutputWorkspace='sum')",EndRunBehavior='Stop',AccumulationWorkspace='acc',OutputWorkspace='ws')\n"""
+
+        script += "sqw_mat = importMatrixWorkspace('ws')\n"
+        script += "sqw_g = sqw_mat.plotGraph2D()\n"
+        script += "sqw_g.activeLayer().setScale(Layer.Right,0.0001,1)\n"
+        script += "sqw_g.activeLayer().logColor()\n"
+
+        script += "ephi_mat = importMatrixWorkspace('Ephi')\n"
+        script += "ephi_g = ephi_mat.plotGraph2D()\n"
+        script += "ephi_l = ephi_g.activeLayer()\n"
+        script += "ephi_l.setScale(Layer.Right,0.0001,1)\n"
+        script += "ephi_l.logColor()\n"
+        script += """ephi_l.setAxisTitle(Layer.Bottom,"Channel number")\n"""
+
+        script += "ephi_1D = plotBin('Ephi',52,True)\n"
+
+        script += "total_counts = plot('sum',0)\n"
+        script += "inst_view = getInstrumentView('reb2')\n"
+        script += "inst_view.show()\n"
+
+        return script
